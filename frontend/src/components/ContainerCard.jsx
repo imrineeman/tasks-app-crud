0b083cb4b2be8b59ab0062c40bde0ee047768f3d
Imri Neeman <neemanimri@gmail.com>
2021-07-31 13:35:37 +0300

    working search bar

--- frontend/src/components/ContainerCard.jsx
+++ frontend/src/components/ContainerCard.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 // Components
 import TaskCard from './TaskCard';
 import Filter from './Filter';
 // Mock data
 import mockData from '../utils/mockdb';
 
-const ContainerCard = () => (
-  <div className="ContainerCard">
-    <Filter />
-    <div>
-      {mockData.map((data) => (
-        <TaskCard
-          name={data.name}
-          description={data.description}
-        />
-      ))}
+const ContainerCard = () => {
+  const [data, setData] = useState(mockData);
+  const [search, setSearch] = useState('');
+  const tasks = search
+    ? data.filter((task) => (task.name.toLowerCase().includes(search.toLowerCase()))) : data;
+
+  return (
+    <div className="ContainerCard">
+      <input
+        onChange={(({ target }) => setSearch(target.value))}
+      />
+      <div>
+        {tasks.map((task) => (
+          <TaskCard
+            name={task.name}
+            description={task.description}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ContainerCard;
--- frontend/src/components/Filter.jsx
+++ frontend/src/components/Filter.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Filter = () => (
   <div className="Filter">
-    Filter!!!
+    <input />
   </div>
 );
 
