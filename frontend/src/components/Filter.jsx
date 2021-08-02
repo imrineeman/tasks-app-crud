7f15468282d773f8c671295138f58c04b031a90e
Imri Neeman <neemanimri@gmail.com>
2021-08-02 22:22:43 +0300

    Improved Filter design

--- frontend/src/components/Filter.jsx
+++ frontend/src/components/Filter.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Input, Button } from 'antd';
+import { PlusCircleFilled, SearchOutlined } from '@ant-design/icons';
 
 const Filter = ({ handleChange, handleClick, toggleVisibility }) => (
   <div className="Filter">
-    <input onChange={handleChange} />
-    <button onClick={handleClick}> New </button>
+    <div className="searchBar">
+      <Input
+        width=""
+        placeholder="Search Tasks"
+        onChange={handleChange}
+        bordered="true"
+        prefix={<SearchOutlined />}
+      />
+    </div>
+    <Button
+      type="primary"
+      ghost
+      shape="circle"
+      size="medium"
+      icon={<PlusCircleFilled />}
+      onClick={handleClick}
+    />
   </div>
 );
 
